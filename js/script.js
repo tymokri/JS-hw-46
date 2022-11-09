93d6bd319d2b83cb68572ba00f95d29e7b56f78c
Kristina Tymofiichuk <99436996+tymokri@users.noreply.github.com>
2022-11-09 12:42:58 +0000

    Fixed (added studentMethods and loop for...in)

--- js/script.js
+++ js/script.js
@@ -11,42 +11,94 @@
         this.marks = new Array(10);
     }
 
-    Student.prototype.getAge = function () {
-        return new Date().getFullYear() - this.birthYear;
+
+    const studentMethods = {
+        getAge: function () {
+            return new Date().getFullYear() - this.birthYear;
+        },
+
+        present: function () {
+            if (this.attendances[this.attendances.length - 1] !== undefined) throw new Error('Attendances array is full');
+
+            for (let i = 0; i < this.attendances.length; i++) {
+                if (this.attendances[i] === undefined) {
+                    this.attendances[i] = true;
+                    break;
+                }
+            }
+        },
+
+        absent: function () {
+            if (this.attendances[this.attendances.length - 1] !== undefined) throw new Error('Attendances array is full');
+
+            for (let i = 0; i < this.attendances.length; i++) {
+                if (this.attendances[i] === undefined) {
+                    this.attendances[i] = false;
+                    break;
+                }
+            }
+        },
+
+        mark: function (value) {
+            if (this.marks[this.marks.length - 1] !== undefined) throw new Error('Marks array is full');
+            if (value > 10) throw new Error('Mark is invalid');
+
+            for (let i = 0; i < this.marks.length; i++) {
+                if (this.marks[i] === undefined) {
+                    this.marks[i] = value;
+                    break;
+                }
+            }
+        },
+
+        summary: function () {
+            const massage1 = "Wow! It's amazing!";
+            const massage2 = "Ok. But you can do it better";
+            const massage3 = "A tad far from perfect! :/";
+
+
+            const indexOfUndefinedAttendances = this.attendances.findIndex( item => {
+                return item === undefined;
+            });
+            if (indexOfUndefinedAttendances !== -1) throw new Error('Based on attendances information course is not completed');
+
+            const indexOfUndefinedMarks = this.marks.findIndex( item => {
+                return item === undefined;
+            });
+            if (indexOfUndefinedMarks !== -1) throw new Error('Based on marks information course is not completed');
+
+
+            let sumMarks = null;
+
+            for (let i = 0; i < this.marks.length; i += 1) {
+                sumMarks += this.marks[i]
+            }
+            let averageMark = sumMarks/this.marks.length;
+
+
+            let averageAttendance = this.attendances.map(item => {
+                if (item) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }).reduce((acc, item) => {
+                return acc + item;
+            })/this.attendances.length;
+
+
+            if (averageMark > 9 && averageAttendance > 0.9) return massage1;
+            if (averageMark < 9 && averageAttendance < 0.9) {
+                return massage3;
+            } else {
+                return massage2;
+            }
+
+        },
     }
 
-    Student.prototype.present = function () {
-        if (this.attendances[this.attendances.length - 1] !== undefined) throw new Error('Attendances array is full');
-
-        for (let i = 0; i < this.attendances.length; i++) {
-            if (this.attendances[i] === undefined) {
-                this.attendances[i] = true;
-                break;
-            }
-        }
-    }
-
-    Student.prototype.absent = function () {
-        if (this.attendances[this.attendances.length - 1] !== undefined) throw new Error('Attendances array is full');
-
-        for (let i = 0; i < this.attendances.length; i++) {
-            if (this.attendances[i] === undefined) {
-                this.attendances[i] = false;
-                break;
-            }
-        }
-    }
-
-    Student.prototype.mark = function (value) {
-        if (this.marks[this.marks.length - 1] !== undefined) throw new Error('Marks array is full');
-        if (value > 10) throw new Error('Mark is invalid');
-
-        for (let i = 0; i < this.marks.length; i++) {
-            if (this.marks[i] === undefined) {
-                this.marks[i] = value;
-                break;
-            }
-        }
+    for (const studentMethodsKey in studentMethods) {
+        Student.prototype[studentMethodsKey] = studentMethods[studentMethodsKey];
     }
 
     let studentJohn = new Student('John', 'Bush', 2005);
@@ -63,65 +115,15 @@
     studentJohn.absent();
 
     studentJohn.mark(9);
-    studentJohn.mark(5);
-    studentJohn.mark(4);
-    studentJohn.mark(10);
-    studentJohn.mark(4);
     studentJohn.mark(8);
-    studentJohn.mark(4);
-    studentJohn.mark(7);
+    studentJohn.mark(8);
     studentJohn.mark(10);
-    studentJohn.mark(5);
-
-
-    Student.prototype.summary = function () {
-        const massage1 = "Wow! It's amazing!";
-        const massage2 = "Ok. But you can do it better";
-        const massage3 = "A tad far from perfect! :/";
-
-
-        const indexOfUndefinedAttendances = this.attendances.findIndex( item => {
-            return item === undefined;
-        });
-        if (indexOfUndefinedAttendances !== -1) throw new Error('Based on attendances information course is not completed');
-
-        const indexOfUndefinedMarks = this.marks.findIndex( item => {
-            return item === undefined;
-        });
-        if (indexOfUndefinedMarks !== -1) throw new Error('Based on marks information course is not completed');
-
-
-        let sumMarks = null;
-
-        for (let i = 0; i < this.marks.length; i += 1) {
-            sumMarks += this.marks[i]
-        }
-        let averageMark = sumMarks/this.marks.length;
-
-
-        let averageAttendance = this.attendances.map(item => {
-            if (item) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }).reduce((acc, item) => {
-            return acc + item;
-        })/this.attendances.length;
-
-
-        console.log( "averageAttendance: " + averageAttendance);
-        console.log( "averageMark:" + averageMark);
-
-
-        if (averageMark > 9 && averageAttendance > 0.9) return massage1;
-        if (averageMark < 9 && averageAttendance < 0.9) {
-            return massage3;
-        } else {
-            return massage2;
-        }
-
-    }
+    studentJohn.mark(10);
+    studentJohn.mark(8);
+    studentJohn.mark(9);
+    studentJohn.mark(10);
+    studentJohn.mark(10);
+    studentJohn.mark(8);
 
     console.log(studentJohn);
     console.log(studentJohn.getAge());
